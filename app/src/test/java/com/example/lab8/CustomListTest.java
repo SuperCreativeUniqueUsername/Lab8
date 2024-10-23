6fe89d0dcd029fa0a5ce6e08b6a9c334d0a02b03
SuperCreativeUniqueUsername <gfrankiw@ualberta.ca>
2024-10-23 17:52:36 -0600

    this is a test that passes

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,12 +12,14 @@ import java.util.ArrayList;
 
 public class CustomListTest {
     private CustomList list;
+
     /**
      * create a mocklist for my citylist
+     *
      * @return
      */
-    public CustomList MockCityList(){
-        list = new CustomList(null,new ArrayList<>());
+    public CustomList MockCityList() {
+        list = new CustomList(null, new ArrayList<>());
         return list;
     }
 
@@ -25,14 +27,13 @@ public class CustomListTest {
      * get the size of the list
      * increase the list by adding a new city
      * check if our current size matches the initial size
-     plus one
+     * plus one
      */
     @Test
-    public void addCityTest(){
+    public void addCityTest() {
         list = MockCityList();
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
-        assertEquals(list.getCount(),listSize + 1);
-
-
+        assertEquals(list.getCount(), listSize + 1);
+    }
 }
